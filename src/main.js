7a791a23a6ef7274cca6fc10e36ca98ea0c677de
TrustingLD <superleo@Mac-CSO-LEO.local>
2026-08-04 08:08:40 +0200

    nightpersonas

--- src/main.js
+++ src/main.js
@@ -66,7 +66,7 @@ scene.add(sun.target);
 /* ---------- Audio ---------- */
 const sfx = createSfx();
 const musicHintEl = document.getElementById('musicHint');
-const music = createMusic(['./luft-mini.mp3', './minicrafter_theme_final.mp3'], musicHintEl);
+const music = createMusic(['./luft-mini.mp3', './minicrafter_theme_final.mp3', './nightpersonas.mp3'], musicHintEl);
 document.getElementById('musicHint').addEventListener('click', music.toggleBgmMute);
 
 /* ---------- Monde ---------- */
